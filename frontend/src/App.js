f916bf2c16a794ed5a97da5811da9f10d8be50d2
sam2420 <100222920+sam2420@users.noreply.github.com>
2022-11-18 17:26:28 +0530

    video-03-create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">website</a>
+      </header>
+      <main>
+        list of products
+        </main>
+    </div>
+  );
+}
+
+export default App;
