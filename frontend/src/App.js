fff2d2da2a20c8ee2d6140a9f7ec1c261339d289
sam2420 <100222920+sam2420@users.noreply.github.com>
2022-11-18 18:24:13 +0530

    video-07

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 function App() {
   return (
@@ -6,8 +7,26 @@ function App() {
         <a href="/">website</a>
       </header>
       <main>
-        list of products
-        </main>
+        {/* list of products
+         */}
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt="product.name" />
+              </a>
+              <div className="product-info">
+              <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+                </a>
+                <p><strong>Rs.{product.price}</strong></p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,53 @@
+const data={
+    products:[
+        {
+            name:'Nike Slim Shirt',
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock:10,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            name:'Adidas Slim Shirt',
+            slug:'Adidas-slim-shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:250,
+            countInStock:20,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            name:'Lacoste Slim Shirt',
+            slug:'Lacoste-slim-shirt',
+            category:'Shirts',
+            image:'/images/p4.jpg',
+            price:220,
+            countInStock:0,
+            brand:'Lacoste',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            name:'Nike Slim Pant',
+            slug:'Nike-slim-pant',
+            category:'Pants',
+            image:'/images/p3.jpg',
+            price:78,
+            countInStock:15,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+        },
+    ]
+};
+export default data;
