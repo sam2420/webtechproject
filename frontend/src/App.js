14befa7fe8ac46d843cd51ac44b4e0fcf3e4d134
sam2420 <100222920+sam2420@users.noreply.github.com>
2022-11-18 23:52:08 +0530

    boostrapreact

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,45 @@
 import { BrowserRouter } from "react-router-dom";
-import {Routes} from "react-router-dom";
-import {Route} from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 function App() {
   // console.log("hello")
   return (
     // 
     <BrowserRouter>
       {/* console.log("hello"), */}
-      <div className="App">
+      <div className="d-flex flex-column site-container">
         <header className="App-header">
-          <Link to="/">website</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand href="#home">Ecommerce Website</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+
+          </Navbar>
+          {/* <Link to="/">website</Link> */}
         </header>
         <main>
           {/* list of products
            */}
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen/>} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
-          
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
