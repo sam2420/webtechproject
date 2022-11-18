ba9419373494936737b896014a29959c98ae531c
sam2420 <100222920+sam2420@users.noreply.github.com>
2022-11-18 19:57:55 +0530

    hookwasnotinvolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,49 @@
 import data from "./data";
-
+import { BrowserRouter } from "react-router-dom";
+import {Routes} from "react-router-dom";
+import {Route} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import React from "react";
 function App() {
+  // console.log("hello")
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">website</a>
-      </header>
-      <main>
-        {/* list of products
-         */}
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt="product.name" />
-              </a>
-              <div className="product-info">
-              <a href={`/product/${product.slug}`}>
-                <p>{product.name}</p>
+    // 
+    <BrowserRouter>
+      {/* console.log("hello"), */}
+      <div className="App">
+        <header className="App-header">
+          <a href="/">website</a>
+        </header>
+        <main>
+          {/* list of products
+           */}
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+          <h1>Featured Products</h1>
+          <div className="products">
+            {data.products.map((product) => (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt="product.name" />
                 </a>
-                <p><strong>Rs.{product.price}</strong></p>
-                <button>Add to Cart</button>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p>
+                    <strong>Rs.{product.price}</strong>
+                  </p>
+                  <button>Add to Cart</button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+            ))}
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
+// console.log("hello");
 
 export default App;
